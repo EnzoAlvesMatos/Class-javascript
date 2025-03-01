d8b8b1470054159b1c3cfce03c8e6cf41f56e5bc
EnzoAlvesMatos <enzoam0909@gmail.com>
2025-03-01 16:10:38 -0300

    lição de javascript

---
+++ Class-javascript/Pessoa.js
@@ -0,0 +1,7 @@
+class Pessoa{
+nome = '';
+idade = 0;
+fala(){
+    console.log(this.nome + 'falando..')
+}
+}
---
+++ Class-javascript/main.js
@@ -0,0 +1,9 @@
+console.log('Pagina carregada')
+let pGohaan = new Pessoa();
+let pVidel = new Pessoa();
+pGohaan.nome = 'Gohan'
+pVidel.nome = 'Videl'
+pGohaan.idade = 22
+pVidel.idade = 23
+pGohaan.fala();
+pVidel.fala();
